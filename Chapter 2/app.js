ebe0588bede4308e91ea6f54a14199a3d3bfb20f
Arsalan Fayyaz <arsalanfayyaz37@gmail.comm>
2025-10-27 12:23:12 -0700

    Added new files or updates

---
+++ Chapter 2/app.js
@@ -0,0 +1,34 @@
+// Q1) Declare a variable called username.
+
+//var userName
+
+// Q2) Declare a variable called myName & assign to it a string 
+//that represents your Full Name.
+
+// var myName = "Arsalan Fayyaz"
+
+// Q3) Write script to 
+// a) Declare a JS variable, titled message. 
+// b) Assign “Hello World” to variable message 
+// c) Display the message in alert box. 
+
+// var message;
+// message = "Hello World"
+// alert(message)
+
+// Q4)Write a script to save student’s bio data in JS variables and 
+// show the data in alert boxes.
+
+// var studentName = "Arsalan Fayyaz";
+// var Age = 23;
+// alert(studentName)
+// alert(Age)
+
+// Q5)  Declare a variable called email and assign to it a string that 
+// represents your Email Address(e.g. example@example.com). 
+// Show the blow mentioned message in an alert box.(Hint: use 
+// string concatenation) 
+
+// var email = "arsalanfayyaz37@gmail.com"
+// alert("My email address is: " + email)
+
