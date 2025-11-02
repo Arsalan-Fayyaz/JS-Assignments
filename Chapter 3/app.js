20ca32f50597a53b14f7eb4d7ae979d71ef41a2c
Arsalan Fayyaz <arsalanfayyaz37@gmail.comm>
2025-11-02 11:37:15 -0800

    Chapter 3

---
+++ Chapter 3/app.js
@@ -0,0 +1,20 @@
+// Q1) Declare a variable called age & assign to it your age. Show 
+// your age in an alert box. 
+
+// var age = 23
+// alert("your age is: " + age)
+
+// Q2) Declare & initialize a variable to keep track of how many 
+// times a visitor has visited a web page. Show his/her 
+// number of visits on your web page. For example: “You 
+// have visited this site N times”.
+
+// var visits = 15
+// alert("You have visited this site" + visits + " times.")
+
+// Q3) Declare a variable called birthYear & assign to it your 
+// birth year. Show the following message in your browser:
+
+// var birthYear = 2002
+// document.writeln("My Birth Year is: " + birthYear)
+
