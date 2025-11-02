fdf43ce364fd1d0a7d66800e883d07d380093813
Arsalan Fayyaz <arsalanfayyaz37@gmail.comm>
2025-11-02 11:47:14 -0800

    Chapter 4

---
+++ Chapter 4/app.js
@@ -0,0 +1,25 @@
+// Q1) Declare 3 variables in one statement.
+
+// var age=23,name="arsalan",gender="Male";
+
+// Q2) Declare 5 legal & 5 illegal variable names.
+
+// Five legal variables
+
+// var age=23;
+// var name="arsalan";
+// var gender="Male";
+// var _class= 10;
+// var age1= 24;
+
+// // Five illegal variables
+
+// var 1age=23;
+// var @name="arsalan";
+// var %gender="Male";
+// var 1_class= 10;
+// var !age= 24;
+
+
+
+
