8a33c5163c473a393a86f5db9dec80c0d866d5ed
Arsalan Fayyaz <arsalanfayyaz37@gmail.comm>
2025-11-03 02:51:48 -0800

    Chapter 5

---
+++ Chapter 5/app.js
@@ -0,0 +1,94 @@
+// Q1) Write a program that take two numbers & add them in a 
+// new variable. Show the result in your browser. 
+
+// var num1 = Number(prompt("Enter First number"));
+// var num2 = Number(prompt("Enter Second number"));
+// var sum = num1 + num2;
+// document.writeln("Sum of " + num1 + " and " + num2 + " is: " + sum);
+
+// Q2) Repeat task1 for subtraction, multiplication, division & 
+// modulus. 
+
+// var num1 = Number(prompt("Enter First number"));
+// var num2 = Number(prompt("Enter Second number"));
+// var sub = num1 - num2;
+// var mul = num1 * num2;
+// var div = num1 / num2;
+// var mod = num1 % num2;
+// document.writeln("sub of " + num1 + " and " + num2 + " is: " + sub + "<br>");
+// document.writeln("Mul of " + num1 + " and " + num2 + " is: " + mul + "<br>");
+// document.writeln("div of " + num1 + " and " + num2 + " is: " + div + "<br>");
+// document.writeln("mod of " + num1 + " and " + num2 + " is: " + mod + "<br>");
+
+// Q3) Cost of one movie ticket is 600 PKR. Write a script to 
+// store ticket price in a variable & calculate the cost of buying 5 
+// tickets. 
+
+// var priceOfTicket = 600;
+// var ticketCal = priceOfTicket * 5;
+// document.writeln("Price of 5 movie tickets are: " + ticketCal)
+
+// Q4) Write a script to display multiplication table of any 
+// number in your browser. 
+
+// var table = 4;
+// document.writeln("4 X 1 = " + table*1 + "<br>")
+// document.writeln("4 X 2 = " + table*2 + "<br>")
+// document.writeln("4 X 3 = " + table*3 + "<br>")
+// document.writeln("4 X 4 = " + table*4 + "<br>")
+// document.writeln("4 X 5 = " + table*5 + "<br>")
+// document.writeln("4 X 6 = " + table*6 + "<br>")
+// document.writeln("4 X 7 = " + table*7 + "<br>")
+// document.writeln("4 X 8 = " + table*8 + "<br>")
+// document.writeln("4 X 9 = " + table*9 + "<br>")
+// document.writeln("4 X 10 = " + table*10 + "<br>")
+
+// Q5) The Temperature Converter: It’s hot out! Let’s make a 
+// converter based on the steps here. 
+// a.  Store a Celsius temperature into a variable. 
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”. 
+
+// var celsiusTemp = 25;
+// var fahrenheitTemp = (celsiusTemp * 9/5) + 32;
+// document.writeln(celsiusTemp+"°C is = " + fahrenheitTemp + "°F")
+
+
+// c. Now store a Fahrenheit temperature into a variable. 
+// d. Convert it to Celsius & output “NNoF is NNoC”.
+
+// var fahrenheitTemp = 70;
+// var celsiusTempTemp = (fahrenheitTemp - 32) * 5/9;
+// document.writeln(fahrenheitTemp+"°F is = " + celsiusTempTemp + "°C")
+
+// Q6) Write a program to implement checkout process of a 
+// shopping cart system for an e-commerce website. Store 
+// the following in variables 
+// a. Price of item 1 
+// b. Price of item 2 
+// c. Ordered quantity of item 1 
+// d. Ordered Quantity of item 2 
+// e. Shipping charges 
+// Compute the total cost & show the receipt in your browser. 
+
+// var priceOfItem1 = 100
+// var priceOfItem2 = 200
+// var quantityOfItem1 = 2
+// var quantityOfItem2 = 3
+// var shippingCharges = 100
+// var totalOfItem1 = priceOfItem1 * quantityOfItem1;
+// var totalOfItem2 = priceOfItem2 * quantityOfItem2;
+// var totalCost = totalOfItem1 + totalOfItem2 + shippingCharges;
+// document.writeln("Total cost of your order is: " + totalCost);
+
+// Q7) Store total marks & marks obtained by a student in 2 
+// variables. Compute the percentage & show the result in 
+// your browser.
+
+// var totalMarks = 500;
+// var obtainedMarks = 450;
+// var percentage = (obtainedMarks / totalMarks) * 100;
+// document.writeln("Total Marks: " + totalMarks + "<br>")
+// document.writeln("Obtained Marks: " + obtainedMarks + "<br>")
+// document.writeln("Percentage: " + percentage + "%" + "<br>")
+
+
